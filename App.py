d0a0c6c4af83bb8625bc06dd250c957da30a8874
Jay-AiTechlabs <s.uba85@yahoo.com>
2026-05-04 13:57:58 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+from chatbot import predict_class, get_response, intents
+import markdown
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index_get():
+    return render_template('base.html') 
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.get_json().get("message")
+    intents_list = predict_class(text)
+    response = get_response(intents_list, intents)
+    # Convert Markdown table to HTML table
+    response = markdown.markdown(response, extensions=['tables'])
+     # Convert \n to <br> for HTML display
+    response = response.replace('\n', '<br>')
+    message = {"answer":response}
+    return jsonify(message)
+
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context)
+
+if __name__ == '__main__':
+    app.run(debug=True)
